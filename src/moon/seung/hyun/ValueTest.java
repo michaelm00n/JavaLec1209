6e383311cf0eb8158bb8f610d19e3e9963ad1172
moon <moon@DESKTOP-SU34KMU>
2018-12-09 15:25:59 +0900

    Init

---
+++ src/moon/seung/hyun/ValueTest.java
@@ -0,0 +1,14 @@
+package moon.seung.hyun;
+
+public class ValueTest {
+
+	public static void main(String[] args) {
+		int aVal = 20;
+		int bVal = 15;
+		int Tot;
+
+		Tot = aVal + bVal;
+		System.out.println("Tot = ");
+	}
+
+}
